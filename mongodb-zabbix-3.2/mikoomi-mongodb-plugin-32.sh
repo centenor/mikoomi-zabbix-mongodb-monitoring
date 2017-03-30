fb23fedb5aecaea34177826c82bcbdd707ba66af
winduzoid@winduzoid.ru <Dmitry Kiselev>
2017-03-30 14:43:16 +0200

    Bug fixes

--- mongodb-zabbix-3.2/mikoomi-mongodb-plugin-32.sh
+++ mongodb-zabbix-3.2/mikoomi-mongodb-plugin-32.sh
@@ -2,5 +2,5 @@
 PATH=$PATH:/etc/zabbix/externalscripts:/opt/zabbix/externalscripts:/opt/zabbix/bin:/home/zabbix/bin:/bin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/local/sbin
 export PATH
 BASE_DIR="`dirname $0`"
-/usr/bin/php $BASE_DIR/mikoomi-mongodb-plugin-34.php $*
+/usr/bin/php $BASE_DIR/mikoomi-mongodb-plugin-32.php $*
 echo 0
